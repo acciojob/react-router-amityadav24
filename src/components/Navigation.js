b4708cbc7fa4878e239f5ff56325f328ad1c4d0a
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-06-29 12:55:52 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <span> | </span>
-      <Link to="/about">About</Link>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </nav>
   );
 };
